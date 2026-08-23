8900557529e43ffc0a59b1333102848ffb6e5447
julvox-sudo <julvox23@gmail.com>
2026-08-23 14:01:25 +0200

    P6.36 wire wishlist reconciliation before CSP hashing

--- scripts/harden-inline-element-csp.js
+++ scripts/harden-inline-element-csp.js
@@ -11,6 +11,7 @@ const { hardenPublicArtifact: hardenAccountHtml } = require('./harden-account-ht
 const { hardenPublicArtifact: hardenBudgetDealHtml } = require('./harden-budget-deal-html');
 const { hardenPublicArtifact: hardenWishlistHtml } = require('./harden-wishlist-html');
 const { hardenPublicArtifact: hardenCommunityClaimHtml } = require('./harden-community-claim-html');
+const { hardenPublicArtifact: reconcileWishlistActivePath } = require('./reconcile-wishlist-active-path');
 
 const MARKER = 'data-julvox-csp="inline-elements-v1"';
 const META_PATTERN = /<meta\s+http-equiv=["']Content-Security-Policy["']\s+data-julvox-csp=["']inline-elements-v1["'][^>]*>/i;
@@ -123,6 +124,7 @@ function main() {
   hardenBudgetDealHtml();
   hardenWishlistHtml();
   hardenCommunityClaimHtml();
+  reconcileWishlistActivePath();
   const source = fs.readFileSync(indexPath, 'utf8');
   const hardened = hardenHtml(source);
   assertHardened(hardened);
