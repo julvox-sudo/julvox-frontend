dadac4533464241fab380e1d5939eb18e4cb19b8
julvox-sudo <julvox23@gmail.com>
2026-08-24 04:54:45 +0200

    build(p6): wire score framing truth before CSP hashing

--- scripts/harden-inline-element-csp.js
+++ scripts/harden-inline-element-csp.js
@@ -38,6 +38,7 @@ const { hardenPublicArtifact: reconcileLegacyPriceComparisonTruth } = require('.
 const { hardenPublicArtifact: reconcilePromoPointsTruth } = require('./reconcile-promo-points-truth');
 const { hardenPublicArtifact: reconcilePromoVoteRatioTruth } = require('./reconcile-promo-vote-ratio-truth');
 const { hardenPublicArtifact: reconcileDealLiveVerificationTruth } = require('./reconcile-deal-live-verification-truth');
+const { hardenPublicArtifact: reconcileScoreFramingTruth } = require('./reconcile-score-framing-truth');
 
 const MARKER = 'data-julvox-csp="inline-elements-v1"';
 const META_PATTERN = /<meta\s+http-equiv=["']Content-Security-Policy["']\s+data-julvox-csp=["']inline-elements-v1["'][^>]*>/i;
@@ -177,6 +178,7 @@ function main() {
   reconcilePromoPointsTruth();
   reconcilePromoVoteRatioTruth();
   reconcileDealLiveVerificationTruth();
+  reconcileScoreFramingTruth();
   const source = fs.readFileSync(indexPath, 'utf8');
   const hardened = hardenHtml(source);
   assertHardened(hardened);
