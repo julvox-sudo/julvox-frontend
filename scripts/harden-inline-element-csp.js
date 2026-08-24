857872ebcb28e3fa7658b2f8466d21dd7bc174ff
julvox-sudo <julvox23@gmail.com>
2026-08-24 21:21:15 +0200

    build(p6): wire community reference-gap truth before CSP hashing

--- scripts/harden-inline-element-csp.js
+++ scripts/harden-inline-element-csp.js
@@ -53,6 +53,7 @@ const { hardenPublicArtifact: reconcileDealOfDayShareHandlerTruth } = require('.
 const { hardenPublicArtifact: reconcileDealOfDayReferenceGapTruth } = require('./reconcile-deal-of-day-reference-gap-truth');
 const { hardenPublicArtifact: reconcileFlashReferenceGapTruth } = require('./reconcile-flash-reference-gap-truth');
 const { hardenPublicArtifact: reconcileTrendsReferenceGapTruth } = require('./reconcile-trends-reference-gap-truth');
+const { hardenPublicArtifact: reconcileCommunityReferenceGapTruth } = require('./reconcile-community-reference-gap-truth');
 
 const MARKER = 'data-julvox-csp="inline-elements-v1"';
 const META_PATTERN = /<meta\s+http-equiv=["']Content-Security-Policy["']\s+data-julvox-csp=["']inline-elements-v1["'][^>]*>/i;
@@ -207,6 +208,7 @@ function main() {
   reconcileDealOfDayReferenceGapTruth();
   reconcileFlashReferenceGapTruth();
   reconcileTrendsReferenceGapTruth();
+  reconcileCommunityReferenceGapTruth();
   const source = fs.readFileSync(indexPath, 'utf8');
   const hardened = hardenHtml(source);
   assertHardened(hardened);
